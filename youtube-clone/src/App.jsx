c4d6b6cdef5ab4d653d0b44024d9490aaec5ba11
lenovo <ihozomerveille@gmail.com>
2026-03-17 08:33:09 +0200

    added contents in navbar.jsx and app.jsx

--- youtube-clone/src/App.jsx
+++ youtube-clone/src/App.jsx
@@ -1,10 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+import Feed from "./pages/Feed";
+import VideoDetails from "./pages/VideoDetails";
+import ChannelDetails from "./pages/ChannelDetails";
+
 function App() {
   return (
-    <div className="flex items-center justify-center h-screen bg-black">
-      <h1 className="text-4xl text-red-500 font-bold">
-        Tailwind is Working 🎉
-      </h1>
-    </div>
+    <BrowserRouter>
+
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Feed />} />
+        <Route path="/video/:id" element={<VideoDetails />} />
+        <Route path="/channel/:id" element={<ChannelDetails />} />
+      </Routes>
+
+    </BrowserRouter>
   );
 }
 
---
+++ youtube-clone/src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+function Navbar() {
+  return (
+    <div className="bg-black text-white p-4 flex justify-between">
+      <h1 className="text-xl font-bold">Video Platform</h1>
+    </div>
+  );
+}
+
+export default Navbar;
